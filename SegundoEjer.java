d6ed10ef15fa0e0c3ef28092068864e29434ca8e
iribel97 <irinaochoa32@gmail.com>
2024-06-25 18:40:34 -0500

    Iniciando en Java

---
+++ SegundoEjer.java
@@ -0,0 +1,20 @@
+public class SegundoEjer {
+
+    public static void main(String[] args){
+        //Declara una variable por cada uno de los siguientes tipos de dato
+        int edad = 24;
+        double altura = 1.70;
+        char sexo = 'F';
+        boolean esEstudiante = true;
+        String nombre = "Irina";
+        long telefono = 1234567890;
+
+        // imprimir por consola
+        System.out.println("Mi nombre es " + nombre + "\n"
+        + "Mi edad es " + edad + "\n"
+        + "Mi altura es " + altura + "\n"
+        + "Mi sexo es " + sexo + "\n"
+        + "Soy estudiante: " + esEstudiante + "\n"
+        + "Mi telefono es " + telefono);
+    }
+}
