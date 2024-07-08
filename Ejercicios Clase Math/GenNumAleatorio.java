d186ff8ecbe6ac066be1c34e9361f220b4f639b7
iribel97 <irinaochoa32@gmail.com>
2024-07-07 19:51:16 -0500

    Actividad: Generando números aleatorios. FINALIZADA

---
+++ Ejercicios Clase Math/GenNumAleatorio.java
@@ -0,0 +1,11 @@
+public class GenNumAleatorio {
+    
+    public static void main(String[] args) {
+
+        // variable
+        int numAleatorio = (int) Math.floor(Math.random() * 355 + 1);
+        System.out.println("Número aleatorio: " + numAleatorio);
+
+    }
+
+}
